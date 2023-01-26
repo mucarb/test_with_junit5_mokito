24f91a6265c48fd301bfb54145d04edbc366a35e
mucarb <murilo.rb60@gmail.com>
2023-01-25 21:32:39 -0300

    Project started

---
+++ src/main/java/br/com/murilo/apiTestMokito/ApiJUnit5MokitoApplication.java
@@ -0,0 +1,13 @@
+package br.com.murilo.apiTestMokito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJUnit5MokitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJUnit5MokitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/murilo/apiTestMokito/ApiJUnit5MokitoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.murilo.apiTestMokito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJUnit5MokitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
