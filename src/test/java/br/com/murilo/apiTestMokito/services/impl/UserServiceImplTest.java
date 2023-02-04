72b54aa85d4a2d23356c32191d1814d997b38c12
mucarb <murilo.rb60@gmail.com>
2023-02-03 21:04:47 -0300

    Test for UserService create method success

--- src/test/java/br/com/murilo/apiTestMokito/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/murilo/apiTestMokito/services/impl/UserServiceImplTest.java
@@ -2,6 +2,7 @@ package br.com.murilo.apiTestMokito.services.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
@@ -86,7 +87,15 @@ class UserServiceImplTest {
 	}
 
 	@Test
-	void testCreate() {
+	void whenCreateThenReturnSuccess() {
+		when(repository.save(any())).thenReturn(user);
+		User response = service.create(userDTO);
+		assertNotNull(response);
+		assertEquals(User.class, response.getClass());
+		assertEquals(ID, response.getId());
+		assertEquals(NAME, response.getName());
+		assertEquals(EMAIL, response.getEmail());
+		assertEquals(PASSWORD, response.getPassword());
 	}
 
 	@Test
