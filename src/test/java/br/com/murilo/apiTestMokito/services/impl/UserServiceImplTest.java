facef591c95bb86cfd2ddcb71d3afe0a1dc5d519
mucarb <murilo.rb60@gmail.com>
2023-02-06 16:49:41 -0300

    Test for UserService update method success

--- src/test/java/br/com/murilo/apiTestMokito/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/murilo/apiTestMokito/services/impl/UserServiceImplTest.java
@@ -114,7 +114,15 @@ class UserServiceImplTest {
 	}
 
 	@Test
-	void testUpdate() {
+	void whenUpdateThenReturnSuccess() {
+		when(repository.save(any())).thenReturn(user);
+		User response = service.update(userDTO);
+		assertNotNull(response);
+		assertEquals(User.class, response.getClass());
+		assertEquals(ID, response.getId());
+		assertEquals(NAME, response.getName());
+		assertEquals(EMAIL, response.getEmail());
+		assertEquals(PASSWORD, response.getPassword());
 	}
 
 	@Test
