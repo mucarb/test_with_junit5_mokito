3665dc8661620fbeb5a1ab9171e2b30e220ba816
mucarb <murilo.rb60@gmail.com>
2023-02-06 22:06:08 -0300

    Test for endpoint update from UserResource class

--- src/test/java/br/com/murilo/apiTestMokito/resources/UserResourceTest.java
+++ src/test/java/br/com/murilo/apiTestMokito/resources/UserResourceTest.java
@@ -91,7 +91,18 @@ class UserResourceTest {
 	}
 
 	@Test
-	void testUpdate() {
+	void whenUpdateThenReturnSuccess() {
+		when(service.update(userDTO)).thenReturn(user);
+		when(mapper.map(any(), any())).thenReturn(userDTO);
+		ResponseEntity<UserDTO> response = resource.update(ID, userDTO);
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals(ResponseEntity.class, response.getClass());
+		assertEquals(UserDTO.class, response.getBody().getClass());
+		assertEquals(ID, response.getBody().getId());
+		assertEquals(NAME, response.getBody().getName());
+		assertEquals(EMAIL, response.getBody().getEmail());
 	}
 
 	@Test
